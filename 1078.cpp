bac67e155a1c02020fe9e6941fc5148ee0d5c8de
chocobo <102609785+imchocobo@users.noreply.github.com>
2022-06-02 14:20:21 -0300

    Update 1078.cpp

---
+++ 1078.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    for (int count = 1; count < 11; count++){
+        int S = count * N;
+    printf("%d x %d = %d\n", count, N, S);
+    }
+    return 0;
+}
