a13d1000fcfc1c54c3e1a772ca854c2610b36341
Augusto Lago <imchocobo08@gmail.com>
2022-06-02 14:07:38 -0300

    uri

---
+++ 1114.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int main(){
+    int x=0;
+    do{
+        scanf("%d", &x);
+        if(x == 2002){
+            printf("Acesso Permitido\n");
+        }
+        else{
+            printf("Senha Invalida\n");
+        }
+    }while (x != 2002);
+        
+return 0;
+}
