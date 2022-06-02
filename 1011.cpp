85485c00c7905a39d97ca03e3a784c1eca867482
chocobo <102609785+imchocobo@users.noreply.github.com>
2022-06-02 14:15:25 -0300

    Update 1011.cpp

---
+++ 1011.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+#define PI 3.14159
+
+int main() {
+    double r, VOLUME;
+    scanf("%lf", &r);
+    VOLUME = (4/3.0)*PI*r*r*r;
+    printf("VOLUME = %.3lf\n",VOLUME);
+    return 0;
+  
+}
