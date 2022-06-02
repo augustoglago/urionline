485de65bbca82adc3e83f32748c22a2226e802b6
chocobo <102609785+imchocobo@users.noreply.github.com>
2022-06-02 14:15:43 -0300

    Update 1017.cpp

---
+++ 1017.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int d, t, v;
+    double l;
+    scanf("%d", &t);
+    scanf("%d", &v);
+    d = v*t;
+    l = d/12.0;
+    printf("%.3lf\n", l);
+    return 0;
+}
