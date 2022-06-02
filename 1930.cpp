1bb1b6d7d090dd7e128dad1e256caac78f2dae48
chocobo <102609785+imchocobo@users.noreply.github.com>
2022-06-02 14:16:27 -0300

    Update 1930.cpp

---
+++ 1930.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int t1, t2, t3, t4, ts;
+    scanf("%d", &t1);
+    scanf("%d", &t2);
+    scanf("%d", &t3);
+    scanf("%d", &t4);
+    ts = (t1 + t2 + t3 + t4 -3);
+    printf("%d\n", ts);
+    return 0;
+}
